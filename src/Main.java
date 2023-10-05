046941e42d10daed30175ba96212b07238a87868
HoangDSR <nvh.02021995@gmail.com>
2023-10-05 14:54:22 +0700

    Done j48 - tree

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,17 @@ public class Main {
 //        model.mineAssociationRule();
 //        System.out.println(model);
         
-        MyFPGrowthModel model = new MyFPGrowthModel(
-                "D:\\Weka-3-8-6\\data\\weather.nominal.arff",
-                "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
-                "-N -R first-last"
-        );
-        model.mineAssociationRule();
+//        MyFPGrowthModel model = new MyFPGrowthModel(
+//                "D:\\Weka-3-8-6\\data\\weather.nominal.arff",
+//                "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
+//                "-N -R first-last"
+//        );
+//        model.mineAssociationRule();
+//        System.out.println(model);
+
+        MyDecisionTreeModel model = new MyDecisionTreeModel("D:\\Weka-3-8-6\\data\\iris.arff", "-C 0.25 -M 2",null);
+        model.buildDecisionTree();
+        model.CrossValidationDecisionTree(10);
         System.out.println(model);
     }
 }
--- src/MyKnowledgeModel.java
+++ src/MyKnowledgeModel.java
@@ -6,6 +6,8 @@ import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NominalToBinary;
 import weka.filters.unsupervised.attribute.NumericToNominal;
 import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.instance.RemovePercentage;
+import weka.filters.unsupervised.instance.Resample;
 
 import java.io.IOException;
 
@@ -16,41 +18,71 @@ public class MyKnowledgeModel {
     Instances dataset;
     String[] model_options;
     String[] data_options;
-    
+    Instances trainSet;
+    Instances testSet;
+
+
     public MyKnowledgeModel() {}
-    
+
     public MyKnowledgeModel(String filename,
                             String model_options,
                             String data_options) throws Exception {
         this.source = new DataSource(filename);
         this.dataset = source.getDataSet();
-        this.model_options = splitOptions(model_options);
-        this.data_options = splitOptions(data_options);
+        if (model_options != null){
+            this.model_options = splitOptions(model_options);
+        }
+       if(data_options != null){
+           this.data_options = splitOptions(data_options);
+       }
+
     }
-    
+
     public Instances removeData(Instances originalData) throws Exception {
         Remove remove = new Remove();
         remove.setOptions(data_options);
         remove.setInputFormat(originalData);
-        return Filter.useFilter(originalData, remove);  
+        return Filter.useFilter(originalData, remove);
     }
-    
     public void SaveData(String filename) throws IOException {
         ArffSaver saver = new ArffSaver();
         saver.setInstances(dataset);
     }
-    
+
     public Instances convertData(Instances originalData) throws Exception {
         NumericToNominal n2n = new NumericToNominal();
         n2n.setOptions(data_options);
         n2n.setInputFormat(originalData);
         return Filter.useFilter(originalData, n2n);
     }
-    
+
     public Instances convert2Binary(Instances originalData) throws Exception {
         NominalToBinary n2b = new NominalToBinary();
         n2b.setOptions(data_options);
+        n2b.setBinaryAttributesNominal(true);
         n2b.setInputFormat(originalData);
-        return Filter.useFilter(originalData, n2b);
+        return Filter.useFilter(originalData,n2b);
+    }
+
+    public Instances divideTrainTest(Instances originalSet, double percent,boolean isTest) throws Exception {
+        RemovePercentage rp = new RemovePercentage();
+        rp.setPercentage(percent);
+        rp.setInvertSelection(isTest);
+        rp.setInputFormat(originalSet);
+
+        return Filter.useFilter(originalSet, rp);
+    }
+
+    public  Instances divideTrainTestR(Instances originalSet, double percent, boolean isTest) throws Exception {
+        Resample rs = new Resample();
+        rs.setNoReplacement(true);
+        rs.setSampleSizePercent(percent);
+        rs.setInvertSelection(isTest);
+        rs.setInputFormat(dataset);
+        return  Filter.useFilter(originalSet,rs);
+    }
+    @Override
+    public String toString(){
+        return  dataset.toSummaryString();
     }
 }
